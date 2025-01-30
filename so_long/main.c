c28a2038a585b1afba8a54dfb02682bbae45ba2d
Diogo Paes Soeiro Avila <dpaes-so@c2r3s7.42lisboa.com>
2025-01-30 16:45:02 +0000

    started animations

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 12:01:00 by dpaes-so          #+#    #+#             */
-/*   Updated: 2025/01/30 15:22:06 by dpaes-so         ###   ########.fr       */
+/*   Updated: 2025/01/30 16:39:29 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,25 @@ int	map_parse(char *map_file, t_map *map)
 	map_check(map);
 	return (1);
 }
-
+int test(t_mlx *mlx)
+{	
+	__clock_t delay;
+	if(mlx->game.delay == 300)
+		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->game.rock.img,(mlx->map.player.x)*96 -96, (mlx->map.player.y)*96-96);
+	if(mlx->game.delay == 600)
+		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->game.coin.img,(mlx->map.player.x)*96 -96, (mlx->map.player.y)*96-96);
+	if(mlx->game.delay == 900)
+		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->game.door.img,(mlx->map.player.x)*96 -96, (mlx->map.player.y)*96-96);
+	if(mlx->game.delay == 1100)
+	{
+		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->game.floor.img,(mlx->map.player.x)*96 -96, (mlx->map.player.y)*96-96);
+		mlx->game.delay = 0;
+	}	
+	mlx->game.delay++;
+	ft_printf("x = %d \n",mlx->map.player.x);
+	ft_printf("y = %d \n",mlx->map.player.y);
+	return (0);
+}
 int	main(int ac, char **av)
 {
 	t_mlx	mlx;
@@ -53,7 +71,8 @@ int	main(int ac, char **av)
 		mlx.mlx = mlx_init();
 		map_gen(&mlx, &mlx.map);
 		mlx_hook(mlx.win, 17, 0, closex, &mlx);
-		mlx_key_hook(mlx.win, keys, &mlx);
+		mlx_key_hook(mlx.win, keys,&mlx);
+		mlx_loop_hook(mlx.mlx,test,&mlx);
 		mlx_loop(mlx.mlx);
 	}
 	else
