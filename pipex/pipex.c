5e2e7a9894dec729c9f89b2c98d4cf878f4be5d7
Diogo Paes Soeiro Avila <dpaes-so@c2r7s6.42lisboa.com>
2025-02-12 17:07:52 +0000

    pipex looking like something

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,30 +6,45 @@
 /*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:23:00 by dpaes-so          #+#    #+#             */
-/*   Updated: 2025/02/12 15:27:10 by dpaes-so         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:36:36 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	pprocess(int *pipefd,char **path,char **av)
+void	pprocess(int *pipefd, char **path, char **av)
 {
-    int i;
-    char *exec;
-    char* argument_list[] = {"ls", "-l", NULL};
+	int		i;
+	char	*exec;
+	char	**argument_list = ft_split(av[2], ' ');
 
-    i = 0;
-    while(path[i])
-    {
-        exec = ft_strjoin(path[i],av[2]);
-        int r = execve(exec,argument_list,NULL);
-        ft_printf("%d\n",r);
-        i++;
-    }
+	i = 0;
+    close(pipefd[0]);
+    dup2(pipefd[1],1);
+    close(pipefd[1]);
+	while (path[i])
+	{
+		exec = ft_strjoin(path[i], argument_list[0]);
+		execve(exec, argument_list, NULL);
+		i++;
+	}
 }
-void	cprocess(int *pipefd,char **path,char **av)
+void	cprocess(int *pipefd, char **path, char **av)
 {
-    
+    int		i;
+	char	*exec;
+	char	**argument_list = ft_split(av[3], ' ');
+
+	i = 0;
+    close(pipefd[1]);
+    dup2(pipefd[0],0);
+    close(pipefd[0]);
+	while (path[i])
+	{
+		exec = ft_strjoin(path[i], argument_list[0]);
+		execve(exec, argument_list, NULL);
+		i++;
+	}
 }
 void	pipex(char **av, int ac, char **path)
 {
@@ -47,9 +62,9 @@ void	pipex(char **av, int ac, char **path)
 	if (pid < 0)
 		exit(0);
 	if (pid == 0)
-		cprocess(pipefd,path,av);
+		cprocess(pipefd, path, av);
 	else
-		pprocess(pipefd,path,av);
+		pprocess(pipefd, path, av);
 }
 int	main(int ac, char **av, char *envp[])
 {
