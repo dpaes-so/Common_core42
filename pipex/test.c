487c001d9966da76a1d328f28afbcf991dddbdc1
Diogo Avila <diogo@MacBook-Air-de-Diogo.local>
2025-02-15 20:06:46 +0000

    simulation done should put this in another repository

--- pipex/test.c
+++ pipex/test.c
@@ -1,62 +1,99 @@
-#include <fcntl.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
-int	main(int ac, char *av[], char *env[])
+#define NUM_PLAYERS 4
+#define WINNING_POSITION 12
+#define NUM_SIMULATIONS 10000000
+
+typedef struct {
+    int number;
+    int position;
+    int can_move;
+} Player;
+
+int get_ai_choice() {
+    int choices[3] = {1, 3, 5};
+    return choices[arc4random_uniform(3)];
+}
+
+int simulate_game()
 {
-	int		pipefd[2];
-	int		pid;
-	char	*cat_args[] = {"/bin/cat", "infile", NULL};
-	char	*grep_args[] = {"/usr/bin/grep", "Lausanne", NULL};
+    Player players[NUM_PLAYERS];
+    int i;
+    int j;
+    players[0].number = 1;
 
-	(void)ac;
-	(void)av;
-	// Create the pipe
-	if (pipe(pipefd) == -1)
-	{
-		perror("pipe failed");
-		return (1);
-	}
-	// Fork the process
-	pid = fork();
-	if (pid < 0)
-	{
-		perror("fork failed");
-		return (1);
-	}
-	if (pid == 0)
-	{
-		// Child process: grep Lausanne
-		close(pipefd[1]); // Close the write end of the pipe
-		// Redirect stdin to read from the pipe
-		if (dup2(pipefd[0], STDIN_FILENO) == -1)
-		{
-			perror("dup2 failed");
-			return (1);
-		}
-		close(pipefd[0]); // Close after duplication
-		// Execute grep
-		execve("/usr/bin/grep", grep_args, env);
-		perror("execve grep failed");
-		return (1);
-	}
-	else
-	{
-		// Parent process: cat infile
-		close(pipefd[0]); // Close the read end of the pipe
-		// Redirect stdout to write into the pipe
-		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-		{
-			perror("dup2 failed");
-			return (1);
-		}
-		close(pipefd[1]); // Close after duplication
-		// Execute cat
-		execve("/bin/cat", cat_args, env);
-		perror("execve cat failed");
-		return (1);
-	}
-	return (0);
+    i = 0;
+    while(i < NUM_PLAYERS)
+    {
+        players[i].position = 0;
+        i++;
+    }
+    while(1)
+    {
+        i = 1;
+        while(i < NUM_PLAYERS)
+        {
+            players[i].number = get_ai_choice();
+            // printf("PLAYER %d  NUMBER IS = %d \n",i,players[i].number);
+            i++;
+        }
+        i = 0;
+        while (i < NUM_PLAYERS)
+        {
+            j =  0;
+            players[i].can_move = 1;
+            while(j < NUM_PLAYERS)
+            {
+                if(players[i].number == players[j].number && i != j)
+                    players[i].can_move = 0;
+                j++;
+            }
+            i++;
+        }
+        i = 0;
+        while(i < NUM_PLAYERS)
+        {
+            // printf("PLAYER %d  CAN MOVE= %d \n",i,players[i].can_move);
+            i++;
+        }
+        i = 0;
+        while(i < NUM_PLAYERS)
+        {
+            if(players[i].can_move == 1)
+            {
+                players[i].position += players[i].number;
+                // printf("PLAYER [%d] MOVED %d SPACES\n",i,players[i].number);
+            }
+            i++;
+        }
+        i = 0;
+        while(i < NUM_PLAYERS)
+        {
+            if(players[i].position >= WINNING_POSITION)
+            {
+                if(i == 0)
+                {
+                    // printf("PLAYER %d WON!!!\n",i);
+                    return 1;
+                }
+                return 0;
+            }
+            i++;
+        }
+    }
+
+}
+
+int main()
+{
+    int result;
+
+    result = 0;
+    for (int i = 0; i < NUM_SIMULATIONS; i++) {
+        result += simulate_game();
+    }
+    printf("REAL PLAYER WON %d TIMES",result);
+    return 0;
 }
