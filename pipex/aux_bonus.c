6441ea1e336575da40a09947b24691b9e70b0d4d
Diogo Paes Soeiro Avila <dpaes-so@c2r7s5.42lisboa.com>
2025-02-19 16:16:34 +0000

    ye

--- pipex/aux_bonus.c
+++ pipex/aux_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   aux.c                                              :+:      :+:    :+:   */
+/*   aux_bonus.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 14:58:03 by dpaes-so          #+#    #+#             */
-/*   Updated: 2025/02/19 11:47:25 by dpaes-so         ###   ########.fr       */
+/*   Updated: 2025/02/19 16:13:03 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,14 @@ char	**path_finder(char **envp)
 	char	*temp;
 	char	**split;
 
-	i = 0;
-	while (envp[i])
-	{
+	i = -1;
+	while (envp[++i])
 		if (ft_strnstr(envp[i], "PATH", 4))
 			break ;
-		i++;
+	if(!envp[i])
+	{
+		ft_printf("Cant find path");
+		exit(0);
 	}
 	envp[i] = envp[i] + 5;
 	split = ft_split(envp[i], ':');
