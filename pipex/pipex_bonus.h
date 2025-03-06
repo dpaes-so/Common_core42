c87ce4dd41931ab7674574075a2835167ec42016
Diogo Paes Soeiro Avila <dpaes-so@c2r5s1.42lisboa.com>
2025-03-06 10:58:46 +0000

    normie done. and doing some final testes, not changing code tho

--- pipex/pipex_bonus.h
+++ pipex/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:23:36 by dpaes-so          #+#    #+#             */
-/*   Updated: 2025/03/05 12:26:34 by dpaes-so         ###   ########.fr       */
+/*   Updated: 2025/03/06 10:44:55 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,17 +31,18 @@ typedef struct s_pipe
 
 // main
 void		file_parse(t_pipe *pipe, char **av, int *i, int ac);
-void	pipex(t_pipe pipet, char *envp[], int i, int *pid_array);
-void	cmdexec(t_pipe pipe, char *envp[], char **argument_list, int *pid_array);
-void	here_doc(t_pipe *pipe);
+void		pipex(t_pipe pipet, char *envp[], int i, int *pid_array);
+void		cmdexec(t_pipe pipe, char *envp[], char **argument_list,
+				int *pid_array);
+void		here_doc(t_pipe *pipe);
 // aux
 void		freetrix(char **matrix);
 char		**path_finder(char **envp, t_pipe pipe);
 void		wait_child(int *pid_array, int ac, t_pipe pipe);
 void		clean(t_pipe pipe);
-//more aux
-void	here_doc_check(t_pipe *pipe, int *i);
-void	last_fork(t_pipe pipe, char **envp, int i);
-//arg split
-char	**ft_arg_split(char const *s, char c);
+// more aux
+void		here_doc_check(t_pipe *pipe, int *i);
+void		last_fork(t_pipe pipe, char **envp, int i);
+// arg split
+char		**ft_arg_split(char const *s, char c);
 #endif
