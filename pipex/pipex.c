7cb3061744f7717583f7176d700db20e601fcb2c
Diogo Paes Soeiro Avila <dpaes-so@c1r1s9.42lisboa.com>
2025-03-28 14:58:12 +0000

    infie not existing now works correctly and true path also working

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 11:42:15 by dpaes-so          #+#    #+#             */
-/*   Updated: 2025/03/06 12:19:14 by dpaes-so         ###   ########.fr       */
+/*   Updated: 2025/03/28 14:56:45 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@ void	cmdexec(t_pipe pipe, char *envp[], char **argument_list, int *pid_array)
 	while (pipe.path[i] && argument_list[0])
 	{
 		exec = ft_strjoin(pipe.path[i], argument_list[0]);
+		if(access(exec,X_OK) < 0)
+		{
+			free(exec);
+			exec = ft_strdup(argument_list[0]);
+		}
 		close(pipe.outfile_fd);
 		execve(exec, argument_list, envp);
 		free(exec);
@@ -30,7 +35,7 @@ void	cmdexec(t_pipe pipe, char *envp[], char **argument_list, int *pid_array)
 	freetrix(pipe.path);
 	free(pid_array);
 	close(pipe.outfile_fd);
-	ft_putstr_fd("Command not found\n", 2);
+	ft_putstr_fd("Pipex: Command not found\n", 2);
 	exit(127);
 }
 
@@ -48,6 +53,10 @@ void	pipex(t_pipe pipet, char *envp[], int i, int *pid_array)
 		close(pipet.pipefd[0]);
 		dup2(pipet.pipefd[1], 1);
 		close(pipet.pipefd[1]);
+		if (j == 1)
+			if (access(pipet.av[1], F_OK | R_OK) < 0)
+				return (close(pipet.outfile_fd), freetrix(pipet.path),
+					free(pid_array), exit(0));
 		cmdexec(pipet, envp, ft_arg_split(pipet.av[i], ' '), pid_array);
 	}
 	else
@@ -68,13 +77,6 @@ void	file_parse(t_pipe *pipe, char **av, int *i, int ac)
 	if (pipe->outfile_fd < 0)
 		return (perror("Error"), free(pipe->pid_array), close(pipe->outfile_fd),
 			exit(0));
-	if (access(av[1], F_OK | R_OK) < 0)
-	{
-		free(pipe->pid_array);
-		perror("Cant access file or it does not exist");
-		close(pipe->outfile_fd);
-		exit(0);
-	}
 }
 
 int	main(int ac, char **av, char *envp[])
@@ -84,12 +86,12 @@ int	main(int ac, char **av, char *envp[])
 
 	if (ac == 5)
 	{
-		pipe.pid_array = malloc(sizeof(int) * (ac - 3));
+		pipe.infile_fd = 0;
+		pipe.pid_array = ft_calloc((ac - 3), sizeof(int));
 		file_parse(&pipe, av, &i, ac);
 		pipe.infile_fd = open(pipe.av[1], O_RDONLY);
-		if (pipe.infile_fd < 0)
-			return (perror(""), free(pipe.pid_array), close(pipe.outfile_fd));
-		dup2(pipe.infile_fd, 0);
+		if (pipe.infile_fd > 0)
+			dup2(pipe.infile_fd, 0);
 		close(pipe.infile_fd);
 		pipe.path = path_finder(envp, pipe);
 		while (++i < pipe.ac - 2)
