14024cf833429c6ba27a29083e064f2602bb2912
Diogo Paes Soeiro Avila <dpaes-so@c1r1s9.42lisboa.com>
2025-03-28 15:03:41 +0000

    coied the changes i did in pipex mandatory to bonus,(infile not existing and true path, still no exit code 126

--- pipex/pipex_bonus.c
+++ pipex/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 11:42:15 by dpaes-so          #+#    #+#             */
-/*   Updated: 2025/03/06 17:30:16 by dpaes-so         ###   ########.fr       */
+/*   Updated: 2025/03/28 15:02:52 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,11 @@ void	cmdexec(t_pipe pipe, char *envp[], char **argument_list, int *pid_array)
 	while (pipe.path[i] && argument_list[0])
 	{
 		exec = ft_strjoin(pipe.path[i], argument_list[0]);
+		if (access(exec, X_OK) < 0)
+		{
+			free(exec);
+			exec = ft_strdup(argument_list[0]);
+		}
 		close(pipe.outfile_fd);
 		execve(exec, argument_list, envp);
 		free(exec);
@@ -71,6 +76,10 @@ void	pipex(t_pipe pipet, char *envp[], int i, int *pid_array)
 		close(pipet.pipefd[0]);
 		dup2(pipet.pipefd[1], 1);
 		close(pipet.pipefd[1]);
+		if (j == 1)
+			if (access(pipet.av[1], F_OK | R_OK) < 0)
+				return (close(pipet.outfile_fd), freetrix(pipet.path),
+					free(pid_array), exit(0));
 		cmdexec(pipet, envp, ft_arg_split(pipet.av[i], ' '), pid_array);
 	}
 	else
@@ -93,13 +102,6 @@ void	file_parse(t_pipe *pipe, char **av, int *i, int ac)
 			exit(0));
 	if (!ft_strncmp(pipe->av[1], "here_doc", 8))
 		here_doc_check(pipe, i);
-	if (access(av[1], F_OK | R_OK) < 0)
-	{
-		free(pipe->pid_array);
-		perror("Cant access file or it does not exist");
-		close(pipe->outfile_fd);
-		exit(0);
-	}
 }
 
 int	main(int ac, char **av, char *envp[])
@@ -109,12 +111,12 @@ int	main(int ac, char **av, char *envp[])
 
 	if (ac > 4)
 	{
-		pipe.pid_array = malloc(sizeof(int) * (ac - 3));
+		pipe.infile_fd = 0;
+		pipe.pid_array = ft_calloc((ac - 3), sizeof(int));
 		file_parse(&pipe, av, &i, ac);
 		pipe.infile_fd = open(pipe.av[1], O_RDONLY);
-		if (pipe.infile_fd < 0)
-			return (perror(""), free(pipe.pid_array), close(pipe.outfile_fd));
-		dup2(pipe.infile_fd, 0);
+		if (pipe.infile_fd > 0)
+			dup2(pipe.infile_fd, 0);
 		close(pipe.infile_fd);
 		pipe.path = path_finder(envp, pipe);
 		while (++i < pipe.ac - 2)
@@ -123,10 +125,10 @@ int	main(int ac, char **av, char *envp[])
 		close(pipe.outfile_fd);
 		last_fork(pipe, envp, i);
 		close(0);
-		wait_child(pipe.pid_array, pipe.ac, pipe);
+		wait_child(pipe.pid_array, pipe.ac,pipe);
 		clean(pipe);
 	}
 	else
-		ft_putstr_fd("Not enough arguments ", 2);
+		ft_putstr_fd("Please input 5 arguments ", 2);
 	return (0);
 }
